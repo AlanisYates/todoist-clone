e982852bf36a22d8abfb191ba3fd24e80285a101
Alanis Yates <yatesalanis@gmail.com>
2020-04-22 20:17:09 -0400

    adding in firebase connection

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = firebase.initalizeApp({
+    apiKey: "AIzaSyCtJqge4l43I0zjry3HoFMBWMN40lLf4KY",
+    authDomain: "todist-tut-24939.firebaseapp.com",
+    databaseURL: "https://todist-tut-24939.firebaseio.com",
+    projectId: "todist-tut-24939",
+    storageBucket: "todist-tut-24939.appspot.com",
+    messagingSenderId: "861916047575",
+    appId: "1:861916047575:web:19aca99210794175f0e892"
+});
+
+export { firebaseConfig as firebase };
