899fb177c80cfc2b6fa347c4535177368b13eae5
Alanis Yates <yatesalanis@gmail.com>
2020-04-23 23:30:16 -0400

    more styling of sidebar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -9,7 +9,8 @@ export const Header = () => {
             </div>
             <div className="settings">
                 <ul>
-                    <li>+</li>
+                    <li data-testid="quick-add-task-action" className="settings__add">+</li>
+                    <li data-testid="dark-mode-action" className="settings__darkmode"></li>
                     <li>
                         <FaPizzaSlice />
                     </li>
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -4,16 +4,33 @@ import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-i
 export const Sidebar = () => (
     <div className="sidebar" data-testid="sidebar">
         <ul className="sidebar__generic">
-            <li><span><FaInbox /></span><span>Inbox</span></li>
-            <li><span><FaRegCalendar /></span><span>Today</span></li>
-            <li><span><FaRegCalendarAlt /></span><span>Next 7 Days</span></li>
+            <li data-testid="inbox" className="inbox">
+                <span>
+                    <FaInbox />
+                </span>
+                <span>Inbox</span>
+            </li>
+            <li data-testid="today" className="today">
+                <span>
+                    <FaRegCalendar />
+                </span>
+                <span>Today</span>
+            </li>
+            <li data-testid="next_7" className="next_7">
+                <span>
+                    <FaRegCalendarAlt />
+                </span>
+                <span>Next 7 Days</span>
+            </li>
         </ul>
         <div className="sidebar__middle">
-            <span><FaChevronDown /></span>
+            <span>
+                <FaChevronDown />
+            </span>
             <h2>Projects</h2>
         </div>
 
         <ul className="sidebar__projects">Projects will be here!</ul>
-        add Project Component Here! 
+        add Project Component Here!
     </div>
 )
